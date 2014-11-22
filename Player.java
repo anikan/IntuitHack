31315587091824ce64a39730ab34e55204b1239f
anikan <anikan33@gmail.com>
2014-11-21 18:09:28 -0800

    First commit, starting stuff

---
+++ Player.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Anikan on 11/21/2014.
+ */
+
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+public class Player {
+    public int monthlyIncome = 5000;
+
+
+    public void budgetMode()
+    {
+
+    }
+
+
+
+}
